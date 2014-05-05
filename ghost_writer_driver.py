bb49a09b7ad5342a1d86a1d32058a21b8c45a4b1
tevnpowers <tevnpowers@gmail.com>
2014-05-05 01:11:25 -0400

    added comments. modified the program to loop over all examples, instead of a specific class...

--- ghost_writer_driver.py
+++ ghost_writer_driver.py
@@ -1,63 +1,101 @@
 from file_parser import *
 from sklearn import svm
+from sklearn.metrics import classification_report, confusion_matrix
+
+# Command Line: python ghost_writer_driver.py Dr_Dre Dr_Dre_GhostWritten
 
 # check for correct parameters
 if len(sys.argv) < 3:
 	print('You must provide the following parameters: 1) Name of directory containing texts written by author one 2) Name of directory containing ghost written texts attributed to author one.')
 	sys.exit()
 
-# determine if the last parameter is a song or directory
+# determine if the first parameter is a directory, if not then notify the user and exit the program
 if os.path.isdir(sys.argv[1]):
 	class1_song_names, class1_songs = get_all_songs(sys.argv[1])
 else:
 	print('Unknown parameter ' + sys.argv[1] + '. Pleaes provide a file name or directory.')
 	sys.exit()
 
-# determine if the last parameter is a song or directory
+# determine if the second parameter is a directory, if not then notify the user and exit the program
 if os.path.isdir(sys.argv[2]):
 	class2_song_names, class2_songs = get_all_songs(sys.argv[2])
 else:
 	print('Unknown parameter ' + sys.argv[2] + '. Pleaes provide a file name or directory.')
 	sys.exit()
 
-# which features to extract
+# which features to extract from each song
+# word rates, character grams, word grams, avg word length, and avg line length
 feature_list = [get_words, get_ngrams, get_word_grams, get_avg_word_length, get_avg_line_length]
 
 # what n should be (only necessary for character gram and word gram extraction
-n_list = [None, 4, 3, None, None,]
+n_list = [None, 3, 2, None, None]
 
+# given a list of lyrics and list of song names, create high-dimensional data points to respresent each point based on the features defined in feature_list
 def create_classification_points(songs, song_names):
+	# list to contain the data point of each song
 	song_points = []
+	
+	# each data point is represented as a list (the list will dynamically grow with rates from each feature)
 	for song in songs:
 		song_points.append([])
 
+	# loop through each feature function in the given list, adding the feature values to the data point for each song
 	for i in range(len(feature_list)):
 		feature_func = feature_list[i]
 
-		# build the representative points for each feature
+		# build the representative points for each feature (the dtm holds the feature data for each song)
 		if feature_func in [get_song_length, get_avg_word_length, get_line_count, get_avg_line_length]:
 			dtm, documents = build_single_feature(song_names, songs, feature_func)
 		else:
 			dtm, vocab, documents = build_feature_vocab(n_list[i], song_names, songs, feature_func)
 
+		# add the values for this feature set to the high dimensional point for each song
 		for j in range(len(dtm)):
 			song_points[j] += dtm[j]
 
+	# return the list of data points representative of each point
 	return song_points
 
+# call the function to create data points for each song
+all_songs = class1_songs + class2_songs
+all_song_names = class1_song_names + class2_song_names
 song_points = create_classification_points(class1_songs+class2_songs, class1_song_names+class2_song_names)
+
+# seperate lists of data points for each class
 class1_song_points = song_points[:len(class1_songs)]
 class2_song_points = song_points[len(class1_songs):]
 
-for i in range(len(class2_songs)):
-	point = class2_song_points[i]
-	name = class2_song_names[i]
-	class2_copy = class2_song_points[:]
-	class2_copy.remove(point)
+predictions = []
+true_labels = [0]*len(class1_songs)+[1]*len(class2_songs)
 
-	training_points = class1_song_points + class2_copy
-	labels = ([0]*len(class1_song_points)) + ([1]*len(class2_copy))
+# for each song in class 2, use SVM to determine if it best fits in class 1 or class 2 (should be class 2)
+for i in range(len(all_songs)):
+	# target point information
+	point = song_points[i]
+	name = all_song_names[i]
+	if i < len(class1_songs):
+		# copy of class 2 points, with the target removed
+		class1_copy = class1_song_points[:]
+		class1_copy.remove(point)
 
+		# training points is the combination of all points minus the target
+		training_points = class1_copy + class2_song_points
+		
+		# label class 1 songs as 0 and class 2 songs as 1
+		labels = ([0]*len(class1_copy)) + ([1]*len(class2_song_points))
+
+	else:
+		# copy of class 2 points, with the target removed
+		class2_copy = class2_song_points[:]
+		class2_copy.remove(point)
+
+		# training points is the combination of all points minus the target
+		training_points = class1_song_points + class2_copy
+	
+		# label class 1 songs as 0 and class 2 songs as 1
+		labels = ([0]*len(class1_song_points)) + ([1]*len(class2_copy))
+
+	# Create multiple SVM variations
 	h = 0.02
 	C = 1.0
 	
@@ -65,10 +103,22 @@ for i in range(len(class2_songs)):
 	rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(training_points, labels)
 	poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(training_points, labels)
 	lin_svc = svm.SVC(C=C).fit(training_points, labels)
+
+	# Predict the label based off of the SVMs above
+	svc_predicted_label = svc.predict([point])[0]
+	rbf_predicted_label = rbf_svc.predict([point])[0]
+	poly_predicted_label = poly_svc.predict([point])[0]
+	lin_predicted_label = lin_svc.predict([point])[0]
 	
-	svc_predicted_label = svc.predict([point])
-	rbf_predicted_label = rbf_svc.predict([point])
-	poly_predicted_label = poly_svc.predict([point])
-	lin_predicted_label = lin_svc.predict([point])
+	predictions.append(svc_predicted_label)
+	print('***'*10)
+	print(svc_predicted_label)
+	print(rbf_predicted_label)
+	print(poly_predicted_label)
+	print(lin_predicted_label)
 	predicted_labels = [svc_predicted_label, rbf_predicted_label, poly_predicted_label, lin_predicted_label]
-	print(predicted_labels)
+
+print('Predicted Labels: ', predictions)
+print('True Labels: ', true_labels)
+print('Confusion Matrix: ', confusion_matrix(true_labels, predictions))
+print('Classification Matrix: ', classification_report(true_labels, predictions))
