87918e65fd082d415cf3be48f7c648979b1ee57f
tevnpowers <tevnpowers@gmail.com>
2014-06-02 06:08:27 -0400

    modified port information

--- comparison.py
+++ comparison.py
@@ -193,6 +193,8 @@ def index():
 	return render_template('index.html', artists=artists)
 
 if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
     app.run(
+		port=port,
         debug=True
     )
